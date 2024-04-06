db0764e4d24990460e344103c1c0765979719181
shesha4572 <shesha4572@gmail.com>
2024-04-07 00:06:51 +0530

    Working mpd cache

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,12 @@
-
 FROM python:3.11
-
 WORKDIR /code
-
 COPY ./requirements.txt /code/requirements.txt
-
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
-
 COPY . /code/app
-
 RUN mkdir /mpd
-
 RUN mkdir /seg
-
 ENV MPD_DIR="/mpd"
-
-EXPOSE 80
-
+EXPOSE 8000
 ENV SEG_CACHE_DIR="/seg"
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+ENV ORIGIN_SERVER_URL="http://192.168.0.1:8080"
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI , Response
 from threading import Lock
 import logging
+import httpx
 
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 
 app = FastAPI()
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("uvicorn")
 mpd_cache = {}
 seg_cache = {}
 mpd_cache_counter = 0
@@ -16,32 +18,79 @@ mpd_cache_lock = Lock()
 seg_cache_lock = Lock()
 mpd_dir = os.getenv("MPD_DIR")
 seg_cache_dir = os.getenv("SEG_CACHE_DIR")
+origin_server_base_url = os.getenv("ORIGIN_SERVER_URL")
+SEG_CACHE_MAX_COUNT = 128
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+async def request_origin(url):
+    client = httpx.AsyncClient()
+    res = await client.get(url)
+    if res.status_code == 200:
+        return res
+    return None
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def save_mpd(content, video_id):
+    logger.info(f"Saving MPD {video_id} to local storage")
+    with open(mpd_dir + "/" + video_id + ".mpd" , "wb") as file:
+        file.write(content)
+    logger.info(f"Successfully saved MPD {video_id} to local storage")
 
-
-async def get_video_mpd_from_origin(videoId):
-    pass
-
-
-@app.get("/mpd/{videoId}")
-async def get_video_mpd(videoId: str):
+def increment_mpd_cache_count(video_id):
     global mpd_cache_counter
-    if videoId in mpd_cache:
-        logger.info("Acquired mpd cache counter lock")
-        mpd_cache_lock.acquire()
-        mpd_cache[videoId] = mpd_cache_counter
-        mpd_cache_counter += 1
-        mpd_cache_lock.release()
-        logger.info("Released mpd cache counter lock")
-        return FileResponse(mpd_dir + "/" + videoId + ".mpd")
+    mpd_cache_lock.acquire()
+    logger.info("Acquired mpd cache counter lock")
+    mpd_cache[video_id] = mpd_cache_counter
+    mpd_cache_counter += 1
+    mpd_cache_lock.release()
+    logger.info("Released mpd cache counter lock")
+
+def increment_seg_cache_count(video_id , bitrate , segment_id):
+    global seg_cache_counter
+    seg_cache_lock.acquire()
+    logger.info("Acquired segment cache lock")
+    seg_cache[(video_id , bitrate ,  segment_id)] = seg_cache_counter
+    seg_cache_counter += 1
+    seg_cache_lock.release()
+    logger.info("Released segment cache lock")
+
+async def get_video_mpd_from_origin(video_id):
+    global mpd_cache_counter
+    logger.info("Contacting Origin Server for MPD " + video_id)
+    res = await request_origin(origin_server_base_url + "/" + video_id + "/manifest.mpd")
+    if res is None:
+        logger.error("Failed to fetch MPD of video " + video_id)
+        raise FileNotFoundError
+    logger.info(f"MPD {video_id} fetched successfully from origin server")
+    increment_mpd_cache_count(video_id)
+    save_mpd(res.content , video_id)
+    return res.content
+
+
+
+@app.get("/{video_id}/manifest.mpd")
+async def get_video_mpd(video_id: str):
+    global mpd_cache_counter
+    if video_id in mpd_cache:
+        logger.info(f"{video_id} MPD cache hit")
+        increment_mpd_cache_count(video_id)
+        response = FileResponse(mpd_dir + "/" + video_id + ".mpd")
+        response.headers["X-Segment-Cache-Hit-Flag"] = "1"
+        return response
+
     else:
-        await get_video_mpd_from_origin(videoId)
-        return get_video_mpd(videoId)
+        logger.info(f"{video_id} MPD cache miss")
+        mpd = await get_video_mpd_from_origin(video_id)
+        response = Response(content=mpd , media_type="application/dash+xml")
+        response.headers["X-Segment-Cache-Hit-Flag"] = "0"
+        return response
+
+@app.get("/{video_id}/seg/{bitrate}/{segment_id}")
+async def get_video_mpd(video_id : str , bitrate : str , segment_id : str):
+    global seg_cache_counter
